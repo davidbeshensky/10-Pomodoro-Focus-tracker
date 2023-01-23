78d941393663d3974e4b7e8ac5e4a45ca5775d4d
David Beshensky <beshenskydave@gmail.com>
2023-01-22 19:48:04 -0600

    saving before adding transitions to the slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import { useState } from 'react';
 import Clock from './components/Clock';
 
-
 function App() {
   
-  const minutesArray = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120];
-  const [index, setIndex] = useState(0);
+  const minutesArray = [0.05, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120];
+  const [index, setIndex] = useState(5);
 
   return (
     <div className="container">
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,37 +2,50 @@
 // the functional component contains a button to increment the timer by 5 minutes, a button to decrement the timer by 5 minutes,
 // and a p tag to display the current timer value
 // the functional component contains a useState hook to keep track of the index of the array containing the timer values
-// the functional component contains two functions, handleIncrement and handleDecrement, to handle the event of a user 
+// the functional component contains two functions, handleIncrement and handleDecrement, to handle the event of a user
 //clicking the increment and decrement buttons, respectively
-
-import React from 'react';
 // this functional component will allow the user to increment the Pomodoro timer by 5 minutes either forward or backward
-import PomoTimer from './PomoTimer';
+import React, { useState } from "react";
+import PomoTimer from "./PomoTimer";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 //timer or clock
-const Clock = ({index, minutesArray, setIndex}) => {
+const Clock = ({ index, minutesArray, setIndex }) => {
+  const minutes = minutesArray[index];
+  const [pomodoros, setPomodoros] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(minutes * 60);
+  const handleIncrement = () => {
+    if (index === minutesArray.length - 1) {
+      return;
+    }
+    setIndex(index + 1);
+  };
 
-    const handleIncrement = () => {
-        if (index === minutesArray.length - 1) {
-            return;
-        }
-        setIndex(index + 1);
-    };
+  const handleDecrement = () => {
+    if (index === 0) {
+      return;
+    }
+    setIndex(index - 1);
+  };
 
-    const handleDecrement = () => {
-        if (index === 0) {
-            return;
-        }
-        setIndex(index - 1);
-    };
+  return (
+    <>
+      <button className="handleDecrement" onClick={handleDecrement}><ArrowBackIosNewIcon />
+      </button>
+      <PomoTimer
+        secondsLeft={secondsLeft}
+        setSecondsLeft={setSecondsLeft}
+        pomodoros={pomodoros}
+        setPomodoros={setPomodoros}
+        index={index}
+        minutesArray={minutesArray}
+      />
+      <button className="handleIncrement" onClick={handleIncrement}>
+        <ArrowForwardIosIcon />
+      </button>
+    </>
+  );
+};
 
-    return (
-        <>
-            <button className='handleDecrement' onClick={handleDecrement}>-</button>
-            <PomoTimer index={index} minutesArray={minutesArray}/>
-            <button className='handleIncrement' onClick={handleIncrement}>+</button>
-        </>
-    );
-    };
-
-    export default Clock;
+export default Clock;
